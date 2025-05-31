c8561a676c414efb8df19571bea4adc97bfd38c9
Gladymar Bello <lalyssbe24@gmail.com>
2025-05-31 17:12:30 -0500

    Actualizado

---
+++ Condicionales.py
@@ -0,0 +1,27 @@
+# age=int(input("Ingresa una edad: "))
+# if age>=18:
+#     print("Es un adulto.")
+# elif age<18:
+#     print("Es un menor de edad.")
+    
+# x=24
+# if x>10:
+#     print("Por encima de diez")
+#     if x>20:
+#         print("Por encima de 20")
+#     else:
+#         print("Pero no por encima de 20")
+        
+
+# a=int(input("Ingresa un número del 1 al 5: "))
+# b=int(input("Igresa un número del 1 al 5: "))
+# c=a*b
+# if c>100:
+#     print("El resultado es mayor a 100")
+# elif c<50:
+#     print("El resultado es menor a cincuenta")
+# elif c>20:
+#     print("El resiltado es mayor a 20")
+# else: 
+#     print("El resultado no cumple con los requisitos")
+
