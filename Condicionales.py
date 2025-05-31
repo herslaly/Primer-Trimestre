50d37b3ffef6dbe2f8994f479bcd04594edd6688
Gladymar Bello <lalyssbe24@gmail.com>
2025-05-31 17:43:55 -0500

    Actualizado

--- Condicionales.py
+++ Condicionales.py
@@ -25,3 +25,17 @@
 # else: 
 #     print("El resultado no cumple con los requisitos")
 
+print("Ejercicio:")
+año=int(input("Ingrese el año de su nacimiento: "))
+if año<=1945:
+    print("Usted es de la generación silenciosa")
+elif año>=1964:
+    print("Usted es de la generación boomer")
+elif año<=1979:
+    print("Usted es de la generación X")
+elif año<=2000:
+    print("Usted es de la generación Y")
+elif año>=2010:
+    print("Usted es de la generación Z")
+else: 
+    print("Usted no nació")   #Aun no esta comprobado
