cb9648cf7dd82e0764fac34173dcee84c75a23a4
Gladymar Bello <lalyssbe24@gmail.com>
2025-06-05 17:15:39 -0500

    Actualizado

---
+++ Condicionales y Operaciones matemáticas.py
@@ -0,0 +1,23 @@
+# print("Ejercicio 1")
+# numero=float(input("Ingresa un número: "))
+# if numero<0:
+#     print("Negativo")
+# elif numero>0:
+#     print("Positivo")
+# elif numero==0:
+#     print("Es igual a cero")
+    
+# print("Ejercicio 2")
+# n2=float(input("Ingresa el priimer número: "))
+# n3=float(input("Ingresar el segundo número: "))
+# if n2<n3:
+#     print(n2," es menor que ",n3)
+# elif n3<n2:
+#     print(n3," es menor que ",n2)
+    
+print("Ejercicio 3")
+n4=int(input("Ingresa un número: "))
+if (n4//2):
+    print("El número es par")
+else:
+    print("El número es impar")
---
+++ Diagramas de flujo.py
@@ -0,0 +1,26 @@
+# print("Ejercicio 1")
+# print("Solicitud de crédito bancario:")
+# usuario=input("Ingresa el nombre de usuario: ")
+# age=int(input("Ingrese su edad correspondiente: "))
+# if age>=18:
+#     print("Cumple los requisitos. ""Recibe el crédito bancario.")
+# else:
+#     print("No cumple con los requisitos.")
+    
+    
+# print("Ejercicio 2")                                     #and=Y or=O not=NO
+# print("Precios para la sala de juegos")
+# aged=int(input("Ingresa la edad del usuario: "))
+# if aged>=0 and aged<=4:
+#     print("Gratis")
+# elif aged>=5 and aged<=18:
+#     print("5 euros")
+# else:
+#     print("18 euros")
+
+print("Ejercicio 3")
+print("Calculadora: Operaciones matemáticas-------Suma:S-----Resta:R-----Multiplicación:M-----División:D")
+S=input("Ingresa un número: ")+(input("Ingresa el segundo número: "))
+opera=input("Ingresa la operación que quiere realizar: ")
+if opera==S:
+    print(S)
